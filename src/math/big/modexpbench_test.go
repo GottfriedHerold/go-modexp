a8ccdb8ded85285e0b3731a5929defcf94fd4a77
Gottfried Herold <gottfried.herold@rub.de>
2025-09-04 21:01:24 +0200

    nat.go: Add specialized modular inverse algorithm for powers of 2 (which is much faster) 	Replace the call in expNNEven to the general modInverse by this algorithm. modexpbench_test.go: Add test for modularInverseModPowerOfTwo

--- src/math/big/modexpbench_test.go
+++ src/math/big/modexpbench_test.go
@@ -479,3 +479,32 @@ func BenchmarkCompareNatExpNNOdd(b *testing.B) {
 		}
 	}
 }
+
+func TestInverseModPowerOfTwo(t *testing.T) {
+	rnd := rand.New(rand.NewSource(99))
+	stk := getStack()
+	defer stk.free()
+	for n := 1; n <= 1024; n++ {
+		powerOfTwo := nat(nil).lsh(nat{1}, uint(n))
+		xs := []nat{nat{1}, nat{3}, nat{1, 1}, nat{0x12345}, nat{0x12345, 12345}}
+		randomX := nat(nil).random(rnd, powerOfTwo, n+1)
+		randomX.setBit(randomX, 0, 1)
+		xs = append(xs, randomX)
+		randomXSquared := nat(nil).sqr(stk, randomX)
+		xs = append(xs, randomXSquared)
+		for _, x := range xs {
+			// Note that we assume result1 to be correct. But we check it, just in case.
+			result1 := nat(nil).make(n/_W + 1)
+			result1 = result1.modInverse(x, powerOfTwo)
+			check := nat(nil).mul(stk, result1, x)
+			check = check.trunc(check, uint(n))
+			if check.cmp(nat{1}) != 0 {
+				t.Fatalf("Error computing modular inverse of %v modulo 2**%v. Result was %v", x, n, result1)
+			}
+			result2 := nat(nil).modularInverseModPowerOfTwo(stk, x, uint(n))
+			if result1.cmp(result2) != 0 {
+				t.Fatalf("Error computing modular inverse of %v modulo 2**%v, result should be %v, but was %v", x, n, result1, result2)
+			}
+		}
+	}
+}
--- src/math/big/nat.go
+++ src/math/big/nat.go
@@ -836,7 +836,7 @@ func (z nat) expNNEven(stk *stack, x, y, m nat) nat {
 	z1 = z1.subMod2N(z1, z2, n)
 
 	// Reuse z2 for p = (z₁ - z₂) [in z1] * m2⁻¹ (mod m₁ [= 2ⁿ]).
-	m2inv := stk.nat(len(m1)).modInverse(m2, m1)
+	m2inv := m1.modularInverseModPowerOfTwo(stk, m2, n) // reuse and invalidate the memory of m1.
 	z2 = z2.mul(stk, z1, m2inv)
 	z2 = z2.trunc(z2, n)
 
@@ -1262,6 +1262,65 @@ func computeMontgomeryk0(m0 Word) (k0 Word) {
 	}
 	k0 = -k0
 	return
+}
+
+// modularInverseModPowerOfTwo computes z := x**(-1) mod 2**n and returns z.
+// z and x may alias, but we make no guarantee about whether we modify x in that case.
+// x must be odd, n must be > 0.
+func (z nat) modularInverseModPowerOfTwo(stk *stack, x nat, n uint) nat {
+
+	// We start by computing x**(-1) mod 2**min(_W, n)
+
+	// We use the same algorithm for this as in computeMontgomeryk0 (Newton-Raphson iteration),
+	// but we may abort earlier if n < _W/2.
+	n0 := min(_W, n)
+
+	k0 := 2 - x[0] // Note that x is odd, so len(x) > 0
+	t := x[0] - 1
+	for i := uint(1); i < n0; i <<= 1 {
+		t *= t
+		k0 *= (t + 1)
+	}
+	if n <= _W {
+		k0 &= (1 << n) - 1
+		return z.setWord(k0)
+	}
+
+	// Otherwise n > _W and k0 equals x**(-1) mod 2**_W.
+
+	numWords := int(n+_W-1) / _W // number of words we need for the final result; this is > 1.
+
+	// Note that if we were to extend Newton-Raphson, we would have needed to compute modulo 2**n throughout the whole computation,
+	// so k0 is less useful.
+
+	if alias(z, x) { // to avoid overwriting x
+		z = nil
+	}
+	z = z.make(numWords + 1)
+
+	// Algorithm from: Dumas, J.G. "On Newton–Raphson
+	// Iteration for Multiplicative Inverses Modulo Prime Powers"
+	// (same source), Hensel Quadratic Modular inverse.
+	// Note that p is 2**_W in the notation of the reference (it works for prime powers).
+	zz := nat(nil).make(2 * numWords)
+	lenX := uint(len(x))
+
+	z.setWord(k0)
+	for i := uint(2); i < uint(numWords); i <<= 1 {
+		zz = zz.sqr(stk, z)
+		zz = zz.trunc(zz, i*_W)
+		zz = zz.mul(stk, zz, x[:min(i, lenX)])
+		zz = zz.trunc(zz, i*_W)
+		z = z.lsh(z, 1)
+		z = z.subMod2N(z, zz, i*_W)
+	}
+	zz = zz.sqr(stk, z)
+	zz = zz.trunc(zz, n)
+	zz = zz.mul(stk, zz, x[:min(uint(numWords), lenX)])
+	zz = zz.trunc(zz, n)
+	z = z.lsh(z, 1)
+	z = z.subMod2N(z, zz, n)
+	return z
 }
 
 func (z nat) expNNOdd(stk *stack, x, y, m nat) nat {
